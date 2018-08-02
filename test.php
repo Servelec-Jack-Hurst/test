20fa009861e4eba26d08a85b0b2ca6af6df98d21
Servelec-Jack-Hurst <42029590+Servelec-Jack-Hurst@users.noreply.github.com>
2018-08-02 14:12:39 +0100

    Added card game

---
+++ test.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+</head>
+<body>
+<?php
+$suits = array (
+	"Spades", "Hearts", "Clubs", "Diamonds"
+);
+
+$faces = array (
+	"Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+	"Nine", "Ten", "Jack", "Queen", "King", "Ace"
+);
+
+$deck = array();
+
+foreach ($suits as $suit)
+{
+	foreach ($faces as $face)
+	{
+		$deck[] = array ("face"=>$face, "suit"=>$suit);
+	}
+}
+
+shuffle($deck);
+
+$card = array_shift($deck);
+
+echo $card['face'] . ' of ' . $card['suit'];
+?>
+<br>
+<br>
+<form method = "POST" action = <?php shuffle($deck)?>>
+	<input type="submit" name = "submit" value = "Draw">
+</body>
+</html>
